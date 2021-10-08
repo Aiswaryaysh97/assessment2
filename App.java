2a29313f0313b65f2a64b76f69895fe65e89cf29
Aiswaryaysh97 <92077116+Aiswaryaysh97@users.noreply.github.com>
2021-10-08 17:09:21 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package com.polymath;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp=new Employee();
+		emp.setEid(10);
+		emp.setEname("Maya");
+		Configuration con=new Configuration()
+		SessionFactory sf=
+		//to store
+		save(emp);
+		
+
+	}
+
+}
